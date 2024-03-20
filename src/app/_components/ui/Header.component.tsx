c8e6c64e1c6e70ba4709ca1feb08d2fe252b4a48
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-03-20 12:03:20 +0000

    Added Header

--- src/app/_components/ui/Header.component.tsx
+++ src/app/_components/ui/Header.component.tsx
@@ -1,32 +1,33 @@
 import Link from "next/link"
+import { Button } from "./button"
 
 
 export default function Header(){
 
     return (
-        <>
-            <nav className="sticky flex items-center pl-2 pr-1 justify-between gap-4 h-[50px] max-w-[500px] top-4 left-[calc((100%-24rem)/2)] p-1 rounded-lg border-2 border-paleBlue">
+        <nav className="sticky top-2 md:top-8 w-full grid place-items-center">
+            <div className=" flex items-center bg-accentBlue pl-6 lg:p-1 lg:pl-4 pr-1 justify-between gap-4 h-[50px] w-[calc(100%-1rem)] md:max-w-[500px] rounded-lg border-b-2 border-2 border-paleBlue shadow">
                 <Link href={'/'}>
                     <p className="text-blue font-semibold">PostEase</p>
                 </Link>
-                <div className="flex gap-3">
-                    <div className="flex items-center gap-2">
+                <div className="flex gap-4">
+                    <div className="hidden sm:flex items-center text-sm gap-4">
                         <Link href={'/pricing'}>
-                            <div>Pricing</div>
+                            <div className="font-light" >Pricing</div>
                         </Link>
                         <Link href={'/devs'}>
-                            <div>Devs</div>
+                            <div className="font-light" >Devs</div>
                         </Link>
                         <Link href={'/curators'}>
-                            <div className="font-light">Curators</div>
+                            <div className="font-light" >Curators</div>
                         </Link>
                     </div>
                     <div className="w-fit h-full">
-                        <button className="w-full bg-blue text-white h-[calc(50px-0.7rem)] px-2 rounded-md overflow-hidden hover:bg-slate-600 duration-150">Join the waitlist</button>
+                        <Button>Join the waitlist</Button>
                     </div>
                 </div>
-            </nav>
-        </>
+            </div>
+        </nav>
     )
 
 }
