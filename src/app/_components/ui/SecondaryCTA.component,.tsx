1681ef484ef3d65c148cb7eb8c02fab358d0d5bb
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-03-20 20:54:53 +0000

    fix: more build issues

--- src/app/_components/ui/SecondaryCTA.component,.tsx
+++ src/app/_components/ui/SecondaryCTA.component,.tsx
@@ -1,18 +1,23 @@
 import { Button } from "./button";
 
-
-
-export default function SecondaryCTA(){
-
+export default function SecondaryCTA() {
     return (
-        <section className="w-full p-8 lg:p-16 grid place-items-center">
+        <section className="grid w-full place-items-center p-8 lg:p-16">
             <div className="flex flex-col gap-2">
-                <h2 className="font-bold text-2xl sm:text-3xl lg:text-4xl lg:w-[500px] text-center mb-3">Get rid of that CMS headache</h2>
-                <p className=" text-2xl text-center font-light mb-2">It'll only take 5 minutes to set up.</p>
-                <a href="https://getwaitlist.com/waitlist/14764" className="m-auto">
+                <h2 className="mb-3 text-center text-2xl font-bold sm:text-3xl lg:w-[500px] lg:text-4xl">
+                    Get rid of that CMS headache
+                </h2>
+                <p className=" mb-2 text-center text-2xl font-light">
+                    It'll only take 5 minutes to set up.
+                </p>
+                <a
+                    href="https://getwaitlist.com/waitlist/14764"
+                    className="m-auto"
+                >
                     <Button>Join Waitlist</Button>
                 </a>
             </div>
         </section>
-    )
+    );
 }
+
