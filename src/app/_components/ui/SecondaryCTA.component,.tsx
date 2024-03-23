465d538d10740e155667431b5255c84a7b5e2837
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-03-23 03:12:54 +0000

    chore: MAGA joke

--- src/app/_components/ui/SecondaryCTA.component,.tsx
+++ src/app/_components/ui/SecondaryCTA.component,.tsx
@@ -4,8 +4,9 @@ export default function SecondaryCTA() {
     return (
         <section className="grid w-full place-items-center p-8 lg:p-16">
             <div className="flex flex-col gap-2">
-                <h2 className="mb-3 text-center text-2xl font-bold sm:text-3xl lg:w-[500px] lg:text-4xl">
-                    Get rid of that CMS headache
+                <h2 className="mb-3 text-center text-2xl font-bold sm:text-3xl lg:w-[500px] lg:text-4xl" data-duh='Yes, this is a MAGA joke.'>
+                    {/* Get rid of that CMS headache */}
+                    We're on a mission to make CMS great again
                 </h2>
                 <p className=" mb-2 text-center text-2xl font-light">
                     It'll only take 5 minutes to set up.
