d22577e89255ebdd47bbcdbf59eec6580e0dcbb8
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-03-25 13:13:00 +0000

    fix: solved preview styles

--- src/app/_components/ui/app/editor/AppEditor.component.tsx
+++ src/app/_components/ui/app/editor/AppEditor.component.tsx
@@ -25,7 +25,7 @@ export default function AppEditorModule(){
                     <Editor changeTitle={changeTitle} changeBody={changeBody} />
                 </div>
                 <div className="w-1/2 rounded-md p-4 bg-white h-full">
-                    <LivePreview postTitle={debouncedTitle} postBodyDebounced={debouncedBody} />
+                    <LivePreview postTitle={debouncedTitle ?? 'New Post Title'} postBodyDebounced={debouncedBody ?? 'New Post Body' } />
                 </div>             
             </div>
         </>
@@ -37,8 +37,8 @@ function Editor({changeTitle, changeBody}: {changeTitle: (s: string)=>void, chan
     return (
         <>
             <div className="flex flex-col gap-4 w-full h-full">
-                <input className="border-b-[1.5px] border-blue p-2 text-xl w-fit min-w-[450px] max-w-[100%] focus:bg-slate-50 focus:border-0" placeholder="Post Title" onChange={(e) => {changeTitle(e.target.value); console.log('hi')}} type="text" required />
-                <textarea className="border-b-[1.5px] border-blue p-2 text-xl w-full h-full focus:bg-slate-50 focus:border-0"  placeholder='Post Body' onChange={(e) => {changeBody(e.target.value)}} />
+                <input className="border-b-[1.5px] border-blue p-2 text-xl w-fit min-w-[450px] max-w-[100%] focus:bg-slate-50 focus:outline-2 focus:outline-slate-200 focus:ring-0" placeholder="Post Title" onChange={(e) => {changeTitle(e.target.value); console.log('hi')}} type="text" required />
+                <textarea className="border-b-[1.5px] border-blue p-2 text-xl w-full h-full focus:bg-slate-50 focus:outline-2 focus:outline-slate-200 focus:ring-0"  placeholder='Post Body' onChange={(e) => {changeBody(e.target.value)}} />
             </div>
         </>
     )
@@ -60,9 +60,9 @@ function LivePreview({postTitle, postBodyDebounced}: {postTitle: string, postBod
 
     return (
         <>
-            <div className="flex flex-col gap-4" style={previewStyles}>
-                <h1 className='text-3xl font-semibold'>{postTitle}</h1>
-                <div className={`preview w-full overflow-x-hidden max-w-[calc(100%-70px)] overflow-y-auto`}>{parse(postBodyHtml.toString())}</div>
+            <div className="flex flex-col gap-4" >
+                <h1 className={`text-3xl font-semibold ${previewStyles.title}`}>{postTitle}</h1>
+                <div className={`preview w-full overflow-x-hidden max-w-[calc(100%-70px)] overflow-y-auto ${previewStyles.body}`}>{parse(postBodyHtml.toString())}</div>
             </div>
         </>
     )
